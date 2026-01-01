265ccb50883cb64217caf8ef87b6b7daafe31c1f
Raki Rahman <mdrakiburrahman@gmail.com>
2026-01-01 10:47:52 +0000

    Installed Spark

--- .devcontainer/overlay/post-attach-commands.sh
+++ .devcontainer/overlay/post-attach-commands.sh
@@ -1,5 +1,26 @@
 #!/bin/bash -e
 
+echo "Starting Apache Spark..."
+
+if pgrep -f "org.apache.spark.deploy.master.Master" >/dev/null; then
+    echo "Spark Master already running"
+else
+    echo "Spark is not running. Starting..."
+    sudo /opt/spark/sbin/start-master.sh
+fi
+
+if pgrep -f "org.apache.spark.deploy.worker.Worker" >/dev/null; then
+    echo "Spark Worker already running"
+else
+    echo "Spark Worker is not running. Starting..."
+    sudo /opt/spark/sbin/start-worker.sh spark://$(hostname):7077
+fi
+
+echo
+echo "----------------------------------"
+echo "Master UI:  http://localhost:8080"
+echo "Workers UI: http://localhost:8081"
+echo "----------------------------------"
 echo
 echo "Post-Attach Commands Complete!"
 echo
--- .devcontainer/overlay/post-create-commands.sh
+++ .devcontainer/overlay/post-create-commands.sh
@@ -1,5 +1,8 @@
-#!/bin/bash -e
+#!/bin/bash -xe
 
-echo
-echo "Post-Create Commands Complete!"
-echo
+GIT_ROOT=$(git rev-parse --show-toplevel)
+
+# Install node packages if this particular repo has a package.json
+[ -f "$GIT_ROOT/package.json" ] && npm install
+
+echo "Hatch (Python): $(hatch --version)"
