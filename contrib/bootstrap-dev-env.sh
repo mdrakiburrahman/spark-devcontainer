e1a55e7c18f385ef82b56ed1d3fbeb688b89b88c
Raki Rahman <mdrakiburrahman@gmail.com>
2026-01-01 06:15:19 +0000

    Dev env bootstrap setup

---
+++ contrib/bootstrap-dev-env.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#
+#
+#       Bootstraps a Linux Devbox host for the VS Code devcontainer idempotently.
+#       If your Devbox restarts, rerun this script.
+#
+# ---------------------------------------------------------------------------------------
+#
+
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+DOCKER_VERSION="5:27.5.1-1~ubuntu.24.04~noble"
+
+if ! [ -x "$(command -v jq)" ]; then
+  echo "jq is not installed on your devbox, installing..."
+  sudo apt-get update >/dev/null && sudo apt-get install -y jq >/dev/null
+fi
+
+if ! [ -x "$(command -v docker)" ]; then
+  echo "docker is not installed on your devbox, installing..."
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  sudo apt-get update -q
+  sudo apt-get install -y apt-transport-https ca-certificates curl
+  sudo apt-get install -y --allow-downgrades docker-ce="$DOCKER_VERSION" docker-ce-cli="$DOCKER_VERSION" containerd.io
+else
+  echo "docker is already installed."
+fi
+
+sudo chmod 666 /var/run/docker.sock
+docker container ls
+docker ps -q | xargs -r docker kill
+
+read -sp "Enter Docker Admin password for rakirahman.azurecr.io: " docker_password
+echo
+echo "$docker_password" | docker login rakirahman.azurecr.io --username rakirahman --password-stdin
+
+if ! [ -x "$(command -v npm)" ]; then
+  echo "npm is not installed on your devbox, installing..."
+  curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+  sudo apt-get update 2>&1 > /dev/null
+  sudo DEBIAN_FRONTEND=noninteractive apt-get install -y nodejs
+else
+  echo "npm is already installed."
+fi
+
+echo "Docker: $(docker --version)"
+echo "npm: $(npm version)"
